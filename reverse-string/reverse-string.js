e5e60b736acaba981c681effa3655a26049a4a5f
DonCastillo <don.qcastillo@yahoo.com>
2020-07-16 15:41:02 -0600

    completed reverse-string exercise

---
+++ reverse-string/reverse-string.js
@@ -0,0 +1,13 @@
+//
+// This is only a SKELETON file for the 'Reverse String' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const reverseString = (string) => {
+  var reverse = '';
+  for(let i=string.length; i>=0; --i ){
+    reverse += string.charAt(i);
+  }
+  return reverse;
+};
+
